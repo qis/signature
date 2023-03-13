9ef6d3004694f53e276dc55ee1999dde712b8c79
Alexej Harm <alexej.h@xiphos.de>
2023-03-13 20:56:49 +0100

    restored example in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,29 @@
-#include "memory.hpp"
 #include <qis/signature.hpp>
-#include <format>
-#include <ranges>
+#include <vector>
 #include <cassert>
+#include <cstring>
 
 static_assert(QIS_SIGNATURE_USE_AVX);
 static_assert(QIS_SIGNATURE_USE_TBB);
 
-#define REQUIRE assert
+int main()
+{
+  // Create a 1 GiB memory block.
+  std::vector<std::uint8_t> memory;
+  memory.resize(1024 * 1024 * 1024, 0);
 
-int main() {}
+  // Write data at the end of the memory block.
+  std::memcpy(memory.data() + memory.size() - 5, "\x01\xCD\xE3\x04\x05", 5);
+
+  // Create signature.
+  const qis::signature search("01 ?? E? 04");
+
+  // Scan memory block.
+  const auto pos = qis::scan(memory.data(), memory.size(), search);
+
+  // Check that the data was found.
+  assert(pos != qis::signature::npos);
+
+  // Check data position.
+  assert(pos == memory.size() - 5);
+}
