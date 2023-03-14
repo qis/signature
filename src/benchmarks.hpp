cbc92c0541715538d589a3f2ea88474076920157
Alexej Harm <alexej.h@xiphos.de>
2023-03-14 18:02:28 +0100

    improved benchmarks

--- src/benchmarks.hpp
+++ src/benchmarks.hpp
@@ -4,16 +4,24 @@
 #include <memory.hpp>
 #include <format>
 #include <vector>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
 
 // clang-format off
 
-#define QIS_BENCHMARK_REGISTER(abi, name) \
+#define QIS_BENCHMARK_REGISTER(abi, name)              \
   BENCHMARK(abi::name)
 
 #define QIS_BENCHMARK(scan, size)                      \
   QIS_BENCHMARK_REGISTER(QIS_SIGNATURE_ABI, signature) \
     ->Name(benchmark_name(scan, size))                 \
     ->ArgPair(size, static_cast<std::int64_t>(scan))
+
+#define QIS_BENCHMARK_ABORT(message)                   \
+  assert(!message);                                    \
+  std::fputs("\nError: " message "\n", stderr);        \
+  std::exit(EXIT_FAILURE);
 
 // clang-format on
 
@@ -28,19 +36,42 @@ using benchmark::kMillisecond;
 
 static std::string benchmark_name(std::size_t type, std::size_t size)
 {
+  if (type < 100 || type > 226 || (type > 126 && type < 200)) {
+    QIS_BENCHMARK_ABORT("Invalid type.");
+  }
+  if (size < 26) {
+    QIS_BENCHMARK_ABORT("Invalid size.");
+  }
   benchmarks.emplace_back(type, size);
   return std::format("{}:{:08X}:{}", type, size, QIS_STRINGIFY_EXPAND(QIS_SIGNATURE_ABI));
 }
 
 static void signature(benchmark::State& state)
 {
+  using benchmark::DoNotOptimize;
+
+  // Handle benchmark parameters.
   const auto size = static_cast<std::size_t>(state.range(0));
   const auto type = static_cast<std::size_t>(state.range(1));
-  const auto memory = mem::get(size).data();
-  const auto search = qis::signature(type > 200 ? mem::scan(type - 200) : mem::find(type - 100));
+  const auto scan = qis::signature(type > 200 ? mem::scan(type - 200) : mem::find(type - 100));
+  const auto data = mem::get(size);
+
+  // Create and initialize data copy.
+  std::vector<std::uint8_t> copy(size);
+  DoNotOptimize(std::memcpy(copy.data(), data.data(), size));
+
+  // Verify data copy and signature.
+  if (const auto pos = qis::scan(copy.data(), size, scan); pos != size - 26) {
+    QIS_BENCHMARK_ABORT("Scan failed.");
+  }
+
+  // Run benchmark.
   for (auto _ : state) {
-    const auto pos = qis::scan(memory, size, search);
-    benchmark::DoNotOptimize(pos);
+    state.PauseTiming();
+    DoNotOptimize(std::memcpy(copy.data(), data.data(), size));
+    state.ResumeTiming();
+    const auto pos = qis::scan(copy.data(), size, scan);
+    DoNotOptimize(pos);
   }
 }
 
