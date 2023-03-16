ad2ac84ff02afab5e134e2d23a6b483f60aa4122
Alexej Harm <alexej.h@xiphos.de>
2023-03-16 18:36:03 +0100

    avx experiments

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,67 @@
-#ifndef __clang__
-#include "simulate.hpp"
-#endif
-#include <immintrin.h>
-#include <array>
-#include <format>
-#include <cstdio>
+#include <qis/signature.hpp>
+#include <print.hpp>
 #include <cstdlib>
 
 namespace qis {
 
-template <std::size_t N>
-void print_array(const std::array<char, N>& a, FILE* stream = stdout) noexcept
+using std::format;
+using std::span;
+
+void test(char* s, std::size_t n, const char* p, std::size_t k)
 {
-  for (std::size_t i = 0; i < N; i++) {
-    if (i) {
-      std::fputc(' ', stream);
-      if (i % 16 == 0) {
-        std::fputs("| ", stream);
-      }
+  print("p: {:>6}{} ", "", span(p, k));
+  print(color::gray, "({})\n", k);
+  print("s: {} ", span(s, n));
+  print(color::red, "{} ..\n", span(s + n, 2));
+
+  /*
+  auto r = npos;
+  switch (k) {
+  case 1:
+    if (const auto it = std::find(s, s + n, p[0]); it != s + n) {
+      return it - s;
+    } else {
+      return r;
     }
-    std::fputs(std::format("{:02X}", static_cast<unsigned char>(a[i])).data(), stdout);
+  case 2:
+    r = avx2_strstr_eq2(s, n, p);
+    break;
+  case 3:
+    r = avx2_strstr_memcmp<3>(s, n, p, memcmp1);
+    break;
+  case 4:
+    r = avx2_strstr_memcmp<4>(s, n, p, memcmp2);
+    break;
+  case 5:
+    r = avx2_strstr_memcmp<5>(s, n, p, memcmp4);
+    break;
+  case 6:
+    r = avx2_strstr_memcmp<6>(s, n, p, memcmp4);
+    break;
+  case 7:
+    r = avx2_strstr_memcmp<7>(s, n, p, memcmp5);
+    break;
+  case 8:
+    r = avx2_strstr_memcmp<8>(s, n, p, memcmp6);
+    break;
+  case 9:
+    r = avx2_strstr_memcmp<9>(s, n, p, memcmp8);
+    break;
+  case 10:
+    r = avx2_strstr_memcmp<10>(s, n, p, memcmp8);
+    break;
+  case 11:
+    r = avx2_strstr_memcmp<11>(s, n, p, memcmp9);
+    break;
+  case 12:
+    r = avx2_strstr_memcmp<12>(s, n, p, memcmp10);
+    break;
+  default:
+    r = avx2_strstr_anysize(s, n, p, k);
+    break;
   }
-  std::fputs("\r\n", stream);
-  std::fflush(stream);
-}
-
-void print(const __m256i& a, FILE* stream = stdout) noexcept
-{
-  std::array<char, 32> dst{};
-  _mm256_storeu_si256(reinterpret_cast<__m256i*>(dst.data()), a);
-  print_array(dst, stream);
-}
-
-void print(const __m128i& a, FILE* stream = stdout) noexcept
-{
-  std::array<char, 32> src{};
-  _mm256_storeu_si256(reinterpret_cast<__m256i*>(src.data()), _mm256_castsi128_si256(a));
-  std::array<char, 16> dst{};
-  std::copy(src.data(), src.data() + 16, dst.data());
-  print_array(dst);
-}
-
-constexpr auto data_s256i0 =
-  "\x11\x12\x13\x14\x15\x16\x17\x18"
-  "\x21\x22\x23\x24\x25\x26\x27\x28"
-  "\x31\x32\x33\x34\x35\x36\x37\x38"
-  "\x41\x42\x43\x44\x45\x46\x47\x48";
-
-constexpr auto data_s256i1 =
-  "\x51\x52\x53\x54\x55\x56\x57\x58"
-  "\x61\x62\x63\x64\x65\x66\x67\x68"
-  "\x71\x72\x73\x74\x75\x76\x77\x78"
-  "\x81\x82\x83\x84\x85\x86\x87\x88";
-
-constexpr auto data_s256i2 =
-  "\x00\x0F\xF0\xFF\x03\x0C\x30\xC0"
-  "\x00\x0F\xF0\xFF\x03\x0C\x30\xC0"
-  "\x00\x0F\xF0\xFF\x03\x0C\x30\xC0"
-  "\x00\x0F\xF0\xFF\x03\x0C\x30\xC0";
-
-constexpr auto data_s256i3 =
-  "\xFF\x0F\xF0\xFF\x03\x0C\x30\xC0"
-  "\x00\xF0\xF0\xFF\x03\x0C\x30\xC0"
-  "\x00\x0F\x0F\x00\x03\x0C\x30\xC0"
-  "\x00\x0F\xF0\xFF\x0C\x0C\x30\xC0";
-
-const auto data_m256i0 = reinterpret_cast<const __m256i*>(qis::data_s256i0);
-const auto data_m256i1 = reinterpret_cast<const __m256i*>(qis::data_s256i1);
-const auto data_m256i2 = reinterpret_cast<const __m256i*>(qis::data_s256i2);
-const auto data_m256i3 = reinterpret_cast<const __m256i*>(qis::data_s256i3);
-
-void test()
-{
-  print(_mm256_castsi128_si256(_mm256_extracti128_si256(_mm256_loadu_si256(data_m256i0), 0)));
+  return r <= n - k ? r : npos;
+  */
 }
 
 }  // namespace qis
@@ -80,7 +69,12 @@ void test()
 int main()
 {
   try {
-    qis::test();
+    std::vector<char> memory(128, 0);
+    qis::signature search{ "DB 27 5B FA FB 5E F1 FC FD FE FD 56 AF" };
+    std::memcpy(memory.data() + 2, search.data(), search.size());
+
+    constexpr std::size_t k = 2;
+    qis::test(memory.data(), 2 + k, search.data(), k);
   }
   catch (const std::exception& e) {
     std::fputs(e.what(), stderr);
---
+++ src/print.hpp
@@ -0,0 +1,96 @@
+#pragma once
+#include <immintrin.h>
+#include <algorithm>
+#include <array>
+#include <format>
+#include <span>
+#include <string>
+#include <string_view>
+#include <cstdio>
+
+namespace qis {
+
+enum class color {
+  gray = 90,
+  red = 91,
+  green = 92,
+  yellow = 93,
+  blue = 94,
+  purple = 95,
+  cyan = 96,
+  white = 97,
+};
+
+void print(const char* s) noexcept
+{
+  std::fputs(s, stdout);
+  std::fflush(stdout);
+}
+
+void print(color c, const char* s) noexcept
+{
+  std::fputs(std::format("\033[0;{:02d}m", static_cast<int>(c)).data(), stdout);
+  std::fputs(s, stdout);
+  std::fputs("\033[0m", stdout);
+  std::fflush(stdout);
+}
+
+template <class Arg, class... Args>
+void print(std::format_string<Arg, Args...> fmt, Arg&& arg, Args&&... args) noexcept
+{
+  print(std::format(fmt, std::forward<Arg>(arg), std::forward<Args>(args)...).data());
+}
+
+template <class Arg, class... Args>
+void print(color c, std::format_string<Arg, Args...> fmt, Arg&& arg, Args&&... args) noexcept
+{
+  print(c, std::format(fmt, std::forward<Arg>(arg), std::forward<Args>(args)...).data());
+}
+
+}  // namespace qis
+
+template <>
+struct std::formatter<std::span<const char>> : std::formatter<std::string_view> {
+  template <class FormatContext>
+  auto format(std::span<const char> data, FormatContext& context) const
+  {
+    std::string s;
+    auto it = std::back_inserter(s);
+    for (std::size_t i = 0, size = data.size(); i < size; i++) {
+      if (i) {
+        *it++ = ' ';
+      }
+      std::format_to(it, "{:02X}", static_cast<unsigned char>(data[i]));
+    }
+    return std::formatter<std::string_view>::format(s, context);
+  }
+};
+
+template <>
+struct std::formatter<__m128i> : std::formatter<std::span<const char>> {
+  template <class FormatContext>
+  auto format(__m128i v, FormatContext& context) const
+  {
+    std::array<char, 32> a256{};
+    _mm256_storeu_si256(reinterpret_cast<__m256i*>(a256.data()), _mm256_castsi128_si256(v));
+    std::array<char, 16> a128{};
+    std::copy(a256.data(), a256.data() + 16, a128.data());
+    std::span s(a128.data(), a128.size());
+    return std::formatter<std::span<const char>>::format(s, context);
+  }
+};
+
+template <>
+struct std::formatter<__m256i> : std::formatter<std::span<const char>> {
+  template <class FormatContext>
+  auto format(__m256i v, FormatContext& context) const
+  {
+    std::array<char, 32> a256{};
+    _mm256_storeu_si256(reinterpret_cast<__m256i*>(a256.data()), v);
+    std::span s(a256.data(), a256.size());
+    return std::formatter<std::span<const char>>::format(s, context);
+  }
+};
+
+template <>
+struct std::formatter<std::span<char>> : std::formatter<std::span<const char>> {};
